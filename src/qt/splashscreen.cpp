495262b3295f25f31ba9fa8557f45f1720d646f3
Tony Mead <tony@localhost>
2026-09-10 09:59:33 +0100

    qt: integrate Xnuva splash and project identity

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -28,91 +28,81 @@
 SplashScreen::SplashScreen(const NetworkStyle* networkStyle)
     : QWidget()
 {
-    // set reference point, paddings
-    int paddingRight            = 50;
-    int paddingTop              = 50;
-    int titleVersionVSpace      = 17;
-    int titleCopyrightVSpace    = 40;
+    float devicePixelRatio = static_cast<QGuiApplication*>(
+        QCoreApplication::instance())->devicePixelRatio();
 
-    float fontFactor            = 1.0;
-    float devicePixelRatio      = 1.0;
-    devicePixelRatio = static_cast<QGuiApplication*>(QCoreApplication::instance())->devicePixelRatio();
+    QString titleText = CLIENT_NAME;
+    QString versionText = QString("Version %1").arg(
+        QString::fromStdString(FormatFullVersion()));
+    const QString& titleAddText = networkStyle->getTitleAddText();
 
-    // define text to place
-    QString titleText       = CLIENT_NAME;
-    QString versionText     = QString("Version %1").arg(QString::fromStdString(FormatFullVersion()));
-    QString copyrightText   = QString::fromUtf8(CopyrightHolders(strprintf("\xc2\xA9 %u-%u ", 2009, COPYRIGHT_YEAR)).c_str());
-    const QString& titleAddText    = networkStyle->getTitleAddText();
+    const QSize logicalSize(960, 540);
+    const QSize pixelSize(
+        qRound(logicalSize.width() * devicePixelRatio),
+        qRound(logicalSize.height() * devicePixelRatio));
 
-    QString font            = QApplication::font().toString();
-
-    // create a bitmap according to device pixelratio
-    QSize splashSize(480*devicePixelRatio,320*devicePixelRatio);
-    pixmap = QPixmap(splashSize);
-
-    // change to HiDPI if it makes sense
+    pixmap = QPixmap(pixelSize);
     pixmap.setDevicePixelRatio(devicePixelRatio);
 
+    QPixmap artwork(":/icons/xnuva_splash");
+
+    if (artwork.isNull()) {
+        pixmap.fill(Qt::black);
+    } else {
+        QPainter backgroundPainter(&pixmap);
+        backgroundPainter.setRenderHint(QPainter::SmoothPixmapTransform, true);
+
+        const QPixmap scaled = artwork.scaled(
+            pixelSize,
+            Qt::KeepAspectRatioByExpanding,
+            Qt::SmoothTransformation);
+
+        const int x = (scaled.width() - pixelSize.width()) / 2;
+        const int y = (scaled.height() - pixelSize.height()) / 2;
+
+        backgroundPainter.drawPixmap(
+            QRect(0, 0, pixelSize.width(), pixelSize.height()),
+            scaled,
+            QRect(x, y, pixelSize.width(), pixelSize.height()));
+    }
+
     QPainter pixPaint(&pixmap);
-    pixPaint.setPen(QColor(100,100,100));
+    pixPaint.setRenderHint(QPainter::Antialiasing, true);
 
-    // draw a slightly radial gradient
-    QRadialGradient gradient(QPoint(0,0), splashSize.width()/devicePixelRatio);
-    gradient.setColorAt(0, Qt::white);
-    gradient.setColorAt(1, QColor(247,247,247));
-    QRect rGradient(QPoint(0,0), splashSize);
-    pixPaint.fillRect(rGradient, gradient);
+    // The approved concept artwork contains an early static version placeholder.
+    // Cover it and render the actual binary version dynamically.
+    // Cover the static concept-art version/network placeholder and
+    // replace it with the live binary version and live startup status.
+    QRect statusPanel(
+        qRound(18 * devicePixelRatio),
+        qRound(446 * devicePixelRatio),
+        qRound(305 * devicePixelRatio),
+        qRound(88 * devicePixelRatio));
 
-    // draw the bitcoin icon, expected size of PNG: 1024x1024
-    QRect rectIcon(QPoint(-150,-122), QSize(430,430));
+    pixPaint.fillRect(statusPanel, QColor(5, 13, 26, 225));
 
-    const QSize requiredSize(1024,1024);
-    QPixmap icon(networkStyle->getAppIcon().pixmap(requiredSize));
+    QFont versionFont = QApplication::font();
+    versionFont.setPixelSize(qRound(14 * devicePixelRatio));
+    versionFont.setBold(true);
 
-    pixPaint.drawPixmap(rectIcon, icon);
+    pixPaint.setFont(versionFont);
+    pixPaint.setPen(Qt::white);
 
-    // check font size and drawing with
-    pixPaint.setFont(QFont(font, 33*fontFactor));
-    QFontMetrics fm = pixPaint.fontMetrics();
-    int titleTextWidth = GUIUtil::TextWidth(fm, titleText);
-    if (titleTextWidth > 176) {
-        fontFactor = fontFactor * 176 / titleTextWidth;
+    QString displayedVersion = versionText;
+    if (!titleAddText.isEmpty()) {
+        displayedVersion += QString(" ") + titleAddText;
     }
 
-    pixPaint.setFont(QFont(font, 33*fontFactor));
-    fm = pixPaint.fontMetrics();
-    titleTextWidth  = GUIUtil::TextWidth(fm, titleText);
-    pixPaint.drawText(pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight,paddingTop,titleText);
+    QRect versionRect = statusPanel.adjusted(
+        qRound(12 * devicePixelRatio),
+        qRound(4 * devicePixelRatio),
+        -qRound(12 * devicePixelRatio),
+        -qRound(54 * devicePixelRatio));
 
-    pixPaint.setFont(QFont(font, 15*fontFactor));
-
-    // if the version string is too long, reduce size
-    fm = pixPaint.fontMetrics();
-    int versionTextWidth  = GUIUtil::TextWidth(fm, versionText);
-    if(versionTextWidth > titleTextWidth+paddingRight-10) {
-        pixPaint.setFont(QFont(font, 10*fontFactor));
-        titleVersionVSpace -= 5;
-    }
-    pixPaint.drawText(pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight+2,paddingTop+titleVersionVSpace,versionText);
-
-    // draw copyright stuff
-    {
-        pixPaint.setFont(QFont(font, 10*fontFactor));
-        const int x = pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight;
-        const int y = paddingTop+titleCopyrightVSpace;
-        QRect copyrightRect(x, y, pixmap.width() - x - paddingRight, pixmap.height() - y);
-        pixPaint.drawText(copyrightRect, Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap, copyrightText);
-    }
-
-    // draw additional text if special network
-    if(!titleAddText.isEmpty()) {
-        QFont boldFont = QFont(font, 10*fontFactor);
-        boldFont.setWeight(QFont::Bold);
-        pixPaint.setFont(boldFont);
-        fm = pixPaint.fontMetrics();
-        int titleAddTextWidth  = GUIUtil::TextWidth(fm, titleAddText);
-        pixPaint.drawText(pixmap.width()/devicePixelRatio-titleAddTextWidth-10,15,titleAddText);
-    }
+    pixPaint.drawText(
+        versionRect,
+        Qt::AlignLeft | Qt::AlignVCenter,
+        displayedVersion);
 
     pixPaint.end();
 
@@ -164,8 +154,8 @@ static void InitMessage(SplashScreen *splash, const std::string &message)
     bool invoked = QMetaObject::invokeMethod(splash, "showMessage",
         Qt::QueuedConnection,
         Q_ARG(QString, QString::fromStdString(message)),
-        Q_ARG(int, Qt::AlignBottom|Qt::AlignHCenter),
-        Q_ARG(QColor, QColor(55,55,55)));
+        Q_ARG(int, Qt::AlignBottom|Qt::AlignLeft),
+        Q_ARG(QColor, QColor(235,245,255)));
     assert(invoked);
 }
 
@@ -226,9 +216,15 @@ void SplashScreen::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
     painter.drawPixmap(0, 0, pixmap);
-    QRect r = rect().adjusted(5, 5, -5, -5);
+    // Keep live initialization/progress text inside the bottom-left
+    // status panel built into the Xnuva splash composition.
+    QRect r(30, 476, 275, 50);
+
+    QFont statusFont = font();
+    statusFont.setPixelSize(13);
+    painter.setFont(statusFont);
     painter.setPen(curColor);
-    painter.drawText(r, curAlignment, curMessage);
+    painter.drawText(r, curAlignment | Qt::TextWordWrap, curMessage);
 }
 
 void SplashScreen::closeEvent(QCloseEvent *event)
